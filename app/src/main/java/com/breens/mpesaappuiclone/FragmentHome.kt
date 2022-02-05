6c15a80ee30ecf39b1cbaa4bd1e7ecb06aebf030
user <user@users-MacBook-Air.local>
2022-02-05 15:45:02 +0300

    Initialize repository

---
+++ app/src/main/java/com/breens/mpesaappuiclone/FragmentHome.kt
@@ -0,0 +1,35 @@
+package com.breens.mpesaappuiclone
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.breens.mpesaappuiclone.databinding.FragmentHomeBinding
+
+class FragmentHome: Fragment(R.layout.fragment_home) {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        hideActionBar()
+        Log.d("HOME SCREEN", "Yes Home Screen")
+        return binding.root
+    }
+
+    private fun hideActionBar() {
+        (activity as AppCompatActivity).supportActionBar?.hide()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/breens/mpesaappuiclone/FragmentSplash.kt
@@ -0,0 +1,45 @@
+package com.breens.mpesaappuiclone
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.breens.mpesaappuiclone.databinding.FragmentSplashBinding
+
+class FragmentSplash: Fragment(R.layout.fragment_splash) {
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        hideActionBar()
+        navigateToHomeScreen()
+        Log.d("SPLASH SCREEN", "Yes Splash Screen")
+        return binding.root
+    }
+
+    private fun navigateToHomeScreen() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_fragmentSplash_to_fragmentHome)
+        },3000)
+    }
+
+    private fun hideActionBar() {
+        (activity as AppCompatActivity).supportActionBar?.hide()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
